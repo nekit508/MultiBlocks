6874587aa93b9847568f6bcc18cd7b68efa0cfa3
VlastnayaFaSolka <64124847+VlastnayaFaSolka@users.noreply.github.com>
2021-08-10 15:49:13 +0500

    Add files via upload

--- src/сontent/blocks/ModBlocks.java
+++ src/сontent/blocks/ModBlocks.java
@@ -6,7 +6,7 @@ import mindustry.world.Block;
 public class ModBlocks implements ContentList {
     public static Block
             // defense
-            leadWall,largeLeadWall,forceWall,magneticShield,
+            leadWall,largeLeadWall,metaglassWall,largeMetaglassWall,siliconWall,largeSiliconWall,forceWall,magneticShield,
             // production
             // multi blocks
             multiSilicon,inner,outer,liquidOuter,liquidInner,multiGraphite,multiMetaglass,multiPowerGenerator,multiLiquid;
