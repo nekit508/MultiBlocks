3e970bc5f903300280e49b2944f117139b72fd1c
VlastnayaFaSolka <64124847+VlastnayaFaSolka@users.noreply.github.com>
2021-08-10 14:33:25 +0500

    Add files via upload

--- src/сontent/blocks/ModDefense.java
+++ src/сontent/blocks/ModDefense.java
@@ -14,42 +14,42 @@ import modClasses.CircleForceProjector;
 public class ModDefense implements ContentList {
     @Override
     public void load() {
-		leadWall = new Wall("lead-wall"){{
+        leadWall = new Wall("lead-wall"){{
             lightningChance = -1f;
             chanceDeflect = -1f;
             health = 400;
             size = 1;
             requirements(Category.defense, ItemStack.with(Items.lead, 8));
-		}};
-		largeLeadWall = new Wall("lead-wall-large"){{
+        }};
+        largeLeadWall = new Wall("lead-wall-large"){{
             lightningChance = -1f;
             chanceDeflect = -1f;
             health = 1600;
             size = 2;
             requirements(Category.defense, ItemStack.with(Items.lead, 32));
         }};
-		metaglassWall = new Wall("metaglass-wall"){{
+        metaglassWall = new Wall("metaglass-wall"){{
             lightningChance = -1f;
             chanceDeflect = -1f;
             health = 20;
             size = 1;
             requirements(Category.defense, ItemStack.with(Items.metaglass, 8));
         }};
-		largeMetaglassWall = new Wall("metaglass-wall-large"){{
+        largeMetaglassWall = new Wall("metaglass-wall-large"){{
             lightningChance = -1f;
             chanceDeflect = -1f;
             health = 80;
             size = 2;
             requirements(Category.defense, ItemStack.with(Items.metaglass, 32));
         }}
-		siliconWall = new Wall("silicon-wall"){{
+        siliconWall = new Wall("silicon-wall"){{
             lightningChance = -1f;
             chanceDeflect = -1f;
             health = 40;
             size = 1;
             requirements(Category.defense, ItemStack.with(Items.silicon, 8));
         }};
-		largeSiliconWall = new Wall("silicon-wall-large"){{
+        largeSiliconWall = new Wall("silicon-wall-large"){{
             lightningChance = -1f;
             chanceDeflect = -1f;
             health = 160;
