6abb13869491254026047176bbedc52566177ecb
VlastnayaFaSolka <64124847+VlastnayaFaSolka@users.noreply.github.com>
2021-08-20 15:07:23 +0500

    added new gases

--- src/сontent/items/ModItems.java
+++ src/сontent/items/ModItems.java
@@ -5,17 +5,27 @@ import mindustry.ctype.ContentList;
 import mindustry.type.Item;
 
 public class ModItems implements ContentList {
-    public static Item deuterium,tritium;
+    public static Item deuterium,tritium,oxygen,hydrogen;
 
     @Override
     public void load() {
         Log.info("Loading items");
-        deuterium = new Item("item-deuterium"){{
+        deuterium = new Item('gas-deuterium"){{
             cost = 1f;
             hardness = 3;
             alwaysUnlocked = false;
         }};
-        tritium = new Item("item-tritium"){{
+        tritium = new Item("gas-tritium"){{
+            cost = 1f;
+            hardness = 3;
+            alwaysUnlocked = false;
+        }};
+        hydrogen = new Item("gas-hydrogen"){{
+            cost = 1f;
+            hardness = 3;
+            alwaysUnlocked = false;
+        }};
+        oxygen = new Item("gas-oxygen"){{
             cost = 1f;
             hardness = 3;
             alwaysUnlocked = false;
