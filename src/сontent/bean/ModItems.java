5437f4c5e1e5a886bbcd2f397f5f35b3be8ee77e
VlastnayaFaSolka <64124847+VlastnayaFaSolka@users.noreply.github.com>
2021-08-23 18:17:44 +0500

    added big bean

---
+++ src/сontent/bean/ModItems.java
@@ -0,0 +1,18 @@
+package сontent.bean;
+
+import arc.util.Log;
+import mindustry.ctype.ContentList;
+import mindustry.type.Item;
+
+public class ModItems implements ContentList {
+    public static Item FaSolka;
+
+    @Override
+    public void load() {
+        Log.info("Loading items");
+        FaSolka = new Item("FasolkaItIsBean"){{
+            cost = 1f;
+            hardness = 10;
+            alwaysUnlocked = false;
+        }};
+}
