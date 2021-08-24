1889b93e01ee5e58974a5bed98d130066c46e4ac
VlastnayaFaSolka <64124847+VlastnayaFaSolka@users.noreply.github.com>
2021-08-24 12:31:53 +0500

    hiding beans

--- src/сontent/blocks/ModBlocks.java
+++ src/сontent/blocks/ModBlocks.java
@@ -21,9 +21,9 @@ public class ModBlocks implements ContentList {
             multiSilicon,multiGraphite,multiMetaglass,multiPowerGenerator,
                     multiLiquid,multiReactor,oilFactory,
             // multi blocks's parts
-            inner,outer,liquidOuter,liquidInner,battery,electricInterface,
-			//bean
-			beanInnerUp,beanInnerDown,beanOuter,beanPart1,beanPart2,beanPart3,beanPart4,multiBigBean;
+            inner,outer,liquidOuter,liquidInner,battery,electricInterface;
+            //bean
+	    //beanInnerUp,beanInnerDown,beanOuter,beanPart1,beanPart2,beanPart3,beanPart4,multiBigBean;
 			
     @Override
     public void load() {
